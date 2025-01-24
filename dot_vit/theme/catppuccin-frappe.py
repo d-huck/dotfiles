fd9d9e16e0cb265006e97ff1920974eb1cba311a
dhuck <davin@tinycact.us>
2025-01-23 23:33:32 -0600

    add vit theme and config

---
+++ dot_vit/theme/catppuccin-frappe.py
@@ -0,0 +1,15 @@
+theme = [
+    ('list-header', '', '', '', '', ''),
+    ('list-header-column', '', '', '', 'light cyan', ''),
+    ('list-header-column-separator', 'black', 'dark gray', '', 'black', ''),
+    ('striped-table-row', '', '', '', '', ''),
+    ('reveal focus', 'black', 'dark blue', 'standout', 'black', 'dark blue'),
+    ('message status', 'white', 'dark blue', 'standout', 'white', 'dark blue'),
+    ('message error', 'white', 'dark red', 'standout', 'dark blue', 'dark red'),
+    ('status', 'dark magenta', 'black', 'standout', 'dark magenta', 'black'),
+    ('flash off', 'black', 'black', 'standout', 'black', 'black'),
+    ('flash on', 'white', 'black', 'standout', 'white', 'black'),
+    ('pop_up', 'white', 'black', '', 'white', 'black'),
+    ('button action', 'white', 'light red', '', 'white', 'light red'),
+    ('button cancel', 'black', 'light gray', '', 'black', 'light gray'),
+]
