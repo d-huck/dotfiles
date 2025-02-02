e9d15ad3923be1a6381dca0b00cad8f43ab9d9a6
Davin L <dhuck@tinycact.us>
2025-02-01 21:35:55 -0600

    better task refreshing and todo going to zellij fix

--- dot_local/share/executable_task_sync.sh
+++ dot_local/share/executable_task_sync.sh
@@ -1,7 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/zsh
 
-pids="$(vit --list-pids)"
-for pid in ${pids}; do
-    kill -SIGUSR1 $pid > /dev/null
+DIR="/tmp/vit_pids"
+
+if [[ ! -d "$DIR" ]]; then
+    exit 1
+fi
+
+for pidFile in $(ls "$DIR"/*.pid); do
+   pid=$(basename $pidFile .pid)
+   kill -SIGUSR1 $pid &> /dev/null
+   if [[ $? -ne 0 ]]; then
+      rm $pidFile
+   fi
 done
-
