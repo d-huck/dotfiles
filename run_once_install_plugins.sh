53f9e1a95d80b66feefd14dbfdc7f06a4d41e16c
Davin L <dhuck@tinycact.us>
2025-03-07 16:44:53 -0600

    add zsh vi and zsh-fzf-history-search

--- run_once_install_plugins.sh
+++ run_once_install_plugins.sh
@@ -10,6 +10,8 @@ git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
+git clone https://github.com/jeffreytse/zsh-vi-mode ${$ZSH_CUSTOM:~/.oh-my-zsh/cusom}/plugins/zsh-vi-mode
+git clone https://github.com/joshskidmore/zsh-fzf-history-search ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-fzf-history-search
 
 # nvim Plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
