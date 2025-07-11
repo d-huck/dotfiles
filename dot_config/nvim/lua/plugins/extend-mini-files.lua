69d8a6d9d22b5471953da942d64806d80999bebe
Davin L <dhuck@tinycact.us>
2025-07-11 09:16:19 -0500

    add additional lazyvim files to sync configs

---
+++ dot_config/nvim/lua/plugins/extend-mini-files.lua
@@ -0,0 +1,26 @@
+return {
+  "echasnovski/mini.files",
+  keys = {
+    {
+      "<leader>e",
+      function()
+        require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+      end,
+      desc = "Open mini.files (directory of current file)",
+    },
+    {
+      "<leader>E",
+      function()
+        require("mini.files").open(vim.uv.cwd(), true)
+      end,
+      desc = "Open mini.files (cwd)",
+    },
+  },
+  opts = {
+    windows = {
+      width_nofocus = 20,
+      width_focus = 50,
+      width_preview = 120,
+    },
+  },
+}
