d910fcdc0df76f2a063e4b327537e7f4e829408f
Davin L <dhuck@tinycact.us>
2025-07-24 16:46:08 -0500

    lsp options

--- dot_config/nvim/lua/plugins/extend-lsp.lua
+++ dot_config/nvim/lua/plugins/extend-lsp.lua
@@ -2,9 +2,23 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
-      pyright = {
+      basedpyright = {
         mason = false,
         autostart = false,
+      },
+      pyright = {
+        mason = true,
+        autostart = true,
+        settings = {
+          python = {
+            analysis = {
+              autoSearchPaths = true,
+              diagnosticMode = "openFilesOnly",
+              useLibraryCodeForTypes = true,
+              typeCheckingMode = "off",
+            },
+          },
+        },
       },
     },
   },
