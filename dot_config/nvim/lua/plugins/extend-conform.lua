ef8ee6f3745a481c2bf69e2e1cbb0b4d60ed7ea7
Davin L <dhuck@tinycact.us>
2026-03-05 09:30:34 -0600

    better formatting options

--- dot_config/nvim/lua/plugins/extend-conform.lua
+++ dot_config/nvim/lua/plugins/extend-conform.lua
@@ -2,8 +2,17 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      ["markdown"] = { "prettier" },
-      ["python"] = { "black" },
+      markdown = { "prettier" },
+      python = { "black", "isort" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      vue = { "prettier" },
+      json = { "prettier" },
+    },
+    {
+      formatters = {
+        black = { args = { "--line-length", "120", "-" } },
+      },
     },
   },
 }
