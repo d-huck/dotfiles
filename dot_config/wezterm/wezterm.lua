6f26cf2ae346e887307646d7416b1c48b3afc9f2
Davin L <dhuck@tinycact.us>
2026-03-05 09:30:04 -0600

    neotest

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -20,5 +20,6 @@ config.color_scheme = "Catppuccin Mocha"
 
 config.tab_bar_at_bottom = false
 config.hide_tab_bar_if_only_one_tab = true
+
 -- Finally, return the configuration to wezterm:
 return config
