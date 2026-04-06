cf9731c0af71ee54723e85896ffcfc8aeb0e8d04
Davin L <dhuck@tinycact.us>
2026-04-06 12:33:02 -0500

    add snacks-gh

---
+++ dot_config/nvim/lua/plugins/snacks-gh.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    gh = {
+      -- your gh configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+}
